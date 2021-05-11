3a5bceac4d32125ad02179162930e964fe03d42d
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2021-05-10 21:18:57 -0300

    making the application componentization and improving the typescript structure the components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface GenreProps {
+  genres: Genre[];
+  selected: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, selected, handleClickButton }: GenreProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
